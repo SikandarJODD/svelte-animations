35363c579640dcc85f85af133e5d33184cec9aff
Kaiyuan Ma <84887870+KaiyuanMa@users.noreply.github.com>
2024-07-19 22:17:53 -0700

    added .prettierrc, removed unused import/variables in examples

--- src/lib/examples/comp/ExampleLinkMovingEffect.svelte
+++ src/lib/examples/comp/ExampleLinkMovingEffect.svelte
@@ -1,54 +1,53 @@
 <script lang="ts">
-  import { spring } from "svelte/motion";
-  import { fade, fly, scale } from "svelte/transition";
+	import { spring } from "svelte/motion";
+	import { scale } from "svelte/transition";
 
-  export let hrefDetails = {
-    href: "https://in.pinterest.com/pin/724375921339895692/",
-    text: "Mumbai City",
-    imgSrc:
-      "https://i.pinimg.com/736x/7e/61/74/7e6174c858a5aa169de033f55fc3050c.jpg",
-    imgAlt: "Mumbai",
-  };
+	export let hrefDetails = {
+		href: "https://in.pinterest.com/pin/724375921339895692/",
+		text: "Mumbai City",
+		imgSrc: "https://i.pinimg.com/736x/7e/61/74/7e6174c858a5aa169de033f55fc3050c.jpg",
+		imgAlt: "Mumbai",
+	};
 
-  let isHover = false;
-  let x = spring(0, {
-    stiffness: 0.1,
-    damping: 0.3,
-  });
-  let y = spring(100, {
-    stiffness: 0.1,
-    damping: 0.3,
-  });
+	let isHover = false;
+	let x = spring(0, {
+		stiffness: 0.1,
+		damping: 0.3,
+	});
+	let y = spring(100, {
+		stiffness: 0.1,
+		damping: 0.3,
+	});
 
-  let linkEffect = (node: HTMLElement) => {
-    node.addEventListener("mouseenter", (e) => {
-      y.set(node.getBoundingClientRect().top - 200);
-      isHover = true;
-    });
-    node.addEventListener("mousemove", (e) => {
-      x.set(e.clientX - 120);
-      // y.set(e.clientY + -200);
-    });
-    node.addEventListener("mouseleave", (e) => {
-      isHover = false;
-    });
-  };
+	let linkEffect = (node: HTMLElement) => {
+		node.addEventListener("mouseenter", (e) => {
+			y.set(node.getBoundingClientRect().top - 200);
+			isHover = true;
+		});
+		node.addEventListener("mousemove", (e) => {
+			x.set(e.clientX - 120);
+		});
+		node.addEventListener("mouseleave", (e) => {
+			isHover = false;
+		});
+	};
 </script>
 
 {#if isHover}
-  <img
-    in:scale={{ duration: 200, delay: 200 }}
-    style="position: absolute; top:{$y}px; left:{$x}px; z-indez: 100;"
-    src={hrefDetails.imgSrc}
-    alt={hrefDetails.imgAlt}
-    class="h-44 w-56 rounded-lg shadow-lg object-cover object-center"
-  />
+	<img
+		in:scale={{ duration: 200, delay: 200 }}
+		out:scale={{ duration: 200 }}
+		style="position: absolute; top:{$y}px; left:{$x}px; z-indez: 100;"
+		src={hrefDetails.imgSrc}
+		alt={hrefDetails.imgAlt}
+		class="h-44 w-56 rounded-lg shadow-lg object-cover object-center"
+	/>
 {/if}
 <a
-  use:linkEffect
-  href={hrefDetails.href}
-  target="_blank"
-  class="text-md underline cursor-pointer mx-1"
+	use:linkEffect
+	href={hrefDetails.href}
+	target="_blank"
+	class="text-md underline cursor-pointer mx-1"
 >
-  {hrefDetails.text}
+	{hrefDetails.text}
 </a>
--- src/lib/examples/comp/ExampleTextHoverBounceEffect.svelte
+++ src/lib/examples/comp/ExampleTextHoverBounceEffect.svelte
@@ -1,54 +1,53 @@
 <script>
-  import Box from "$lib/components/dev/tags/Box.svelte";
-  import { Motion } from "svelte-motion";
+	import { Motion } from "svelte-motion";
 
-  let heading = "Coding";
-  let text = heading.split("");
+	let heading = "Coding";
+	let text = heading.split("");
 </script>
 
-  <div class="bg-neutral-950 dark:bg-white/95 p-4 md:p-8 w-full ">
-    <div class="flex items-center justify-center h-56">
-      <Motion initial="initial" whileHover="whileHover" let:motion>
-        <a
-          href="/"
-          use:motion
-          class="group relative flex items-center justify-center transition-colors duration-500"
-        >
-          <div>
-            <Motion
-              variants={{
-                initial: { x: 0 },
-                whileHover: { x: -16 },
-              }}
-              transition={{
-                type: "spring",
-                staggerChildren: 0.075,
-                delayChildren: 0.25,
-              }}
-              let:motion
-            >
-              <span
-                class="relative z-10 block text-4xl font-bold text-neutral-500 transition-colors duration-500 group-hover:text-neutral-50 dark:group-hover:text-black md:text-5xl"
-                use:motion
-              >
-                {#each text as l (l)}
-                  <Motion
-                    variants={{
-                      initial: { x: 0 },
-                      whileHover: { x: 15 },
-                    }}
-                    transition={{ type: "spring" }}
-                    let:motion
-                  >
-                    <span class="inline-block p-0 -mx-1.5" use:motion>
-                      {l}
-                    </span>
-                  </Motion>
-                {/each}
-              </span>
-            </Motion>
-          </div>
-        </a>
-      </Motion>
-    </div>
-  </div>
+<div class="bg-neutral-950 dark:bg-white/95 p-4 md:p-8 w-full">
+	<div class="flex items-center justify-center h-56">
+		<Motion initial="initial" whileHover="whileHover" let:motion>
+			<a
+				href="/"
+				use:motion
+				class="group relative flex items-center justify-center transition-colors duration-500"
+			>
+				<div>
+					<Motion
+						variants={{
+							initial: { x: 0 },
+							whileHover: { x: -16 },
+						}}
+						transition={{
+							type: "spring",
+							staggerChildren: 0.075,
+							delayChildren: 0.25,
+						}}
+						let:motion
+					>
+						<span
+							class="relative z-10 block text-4xl font-bold text-neutral-500 transition-colors duration-500 group-hover:text-neutral-50 dark:group-hover:text-black md:text-5xl"
+							use:motion
+						>
+							{#each text as l (l)}
+								<Motion
+									variants={{
+										initial: { x: 0 },
+										whileHover: { x: 15 },
+									}}
+									transition={{ type: "spring" }}
+									let:motion
+								>
+									<span class="inline-block p-0 -mx-1.5" use:motion>
+										{l}
+									</span>
+								</Motion>
+							{/each}
+						</span>
+					</Motion>
+				</div>
+			</a>
+		</Motion>
+	</div>
+</div>
