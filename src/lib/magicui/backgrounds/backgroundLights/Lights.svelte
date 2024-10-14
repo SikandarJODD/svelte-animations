5e470ce91d1d0ac330f18ff9ee3c75a20d48baa5
Othavio Quiliao <othavioaqa@gmail.com>
2024-10-14 14:20:31 -0300

    Options for top and bottom in the lights component

--- src/lib/magicui/backgrounds/backgroundLights/Lights.svelte
+++ src/lib/magicui/backgrounds/backgroundLights/Lights.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
-  import { cn } from "$lib/utils";
+  import { cn } from '$lib/utils';
 
-  let className: any = "";
-  
+   type Direction = 'top' | 'bottom';
+
+  // Export the 'direction' property
+  export let direction: Direction = 'bottom';
+
+  let className: any = '';
+
   export { className as class };
+
+  // Reactive variables to adjust gradient and position based on direction
+  $: gradientDirection = direction === 'top' ? '0deg' : '180deg';
+  $: positionClass = direction === 'top' ? 'top-[-200px]' : 'bottom-[-200px]';
 </script>
 
-<div class={cn("w-full h-full overflow-hidden", className)}>
+<div class={cn('w-full h-full overflow-hidden', className)}>
   <div
-    style="background:conic-gradient(from 180deg at 50% 50%,var(--blue-500) 0deg,var(--cyan-400) 180deg,var(--yellow-400) 1turn); filter:blur(75px); opacity:20%;"
-    class="w-full h-full relative bottom-[-200px] "
-  />
+    style={`background: conic-gradient(from ${gradientDirection} at 50% 50%, var(--blue-500) 0deg, var(--cyan-400) 180deg, var(--yellow-400) 1turn); filter: blur(75px); opacity: 20%;`}
+    class={`w-full h-full relative ${positionClass}`}
+  ></div>
 </div>
